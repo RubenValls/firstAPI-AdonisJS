c31f7c3fb03b7251acc3ca8d7d132933de4dbd3d
Rubén Valls <r.vallsaparici@gmail.com>
2023-02-27 19:23:12 +0100

    learningsites controller

---
+++ app/Controllers/Http/LearningsitesController.ts
@@ -0,0 +1,28 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class LearningsitesController {
+  public async index(ctx: HttpContextContract) {
+    return [
+      {
+        id: "OpenBootcamp",
+        site: 'https://campus.open-bootcamp.com/',
+        youCanLearn: ["Front-end", "Back-end", "Full-stack", "Mobile"]
+      },
+      {
+        id: "HackerRank",
+        site: 'https://www.hackerrank.com/',
+        youCanLearn: ["Algorithms", "SQL", "Data Structures"]
+      },
+      {
+        id: "freeCodeCamp",
+        site: 'https://www.freecodecamp.org/learn/',
+        youCanLearn: ["Responsive Web Design", "Front-end", "Back-end", "Data Analysis"]
+      },
+      {
+        id: "OpenWebinars",
+        site: 'https://openwebinars.net/academia/',
+        youCanLearn: ["DevOps", "Front-end", "Back-end", "Big Data"]
+      }
+    ]
+  }
+}
--- start/routes.ts
+++ start/routes.ts
@@ -21,5 +21,5 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', async () => {
-  return { hello: 'world', goodbye: 'world'}
+  return {hello: 'world', goodbye: 'world'}
 })
